43324f829456eccff796dc27182e9742860e1f16
alencabira <dbalencastro@gmail.com>
2023-03-23 15:31:33 -0300

    inicial

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: false,
+            },
+            cep: {
+                required: true,
+            }
+        }, 
+        submitHandler: function(form) {
+            alert ('Formulário enviado com sucesso'),
+            reload()
+        }
+
+    })
+
+
+})
